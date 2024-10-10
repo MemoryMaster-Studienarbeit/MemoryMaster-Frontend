ddd525d7cdcea341378d95cf8130a2f87090fd70
Niklas Krauth <nk@syndikat7.de>
2024-10-10 14:59:18 +0200

    Removed unused content/changed used content
    Took 19 minutes

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {AppContainer} from "./App.styles";
+
+const App = () => {
+
+  return (
+    <AppContainer>
+      <h1>Hello World</h1>
+    </AppContainer>
+  )
+
+}
+
+export default App;
